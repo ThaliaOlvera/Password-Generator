612cc3c76c2222519cda94a4ce9ea0f1995b03dc
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2024-09-21 19:37:53 -0500

    Add function to generate random passwords with special characters, numbers, and letters

---
+++ script.js
@@ -0,0 +1,24 @@
+const form = document.getElementById("formContainer");
+const quantity = document.getElementById("length");
+const generate = document.getElementById("ganeratebtn");
+const copy = document.getElementById("copybtn");
+const clearText = document.getElementById("clearbtn");
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~|}{[]:;?><,./-=";
+const showResult = document.getElementById("result");
+
+function password() {
+  if (parseInt(quantity.value) < 9) {
+    alert("Enter a value greater than 9");
+    return;
+  }
+  let digitalNumber = parseInt(quantity.value);
+  let result = "";
+
+  for (let i = 0; i < digitalNumber; i++) {
+    let randomPassword = Math.floor(Math.random() * characters.length);
+    result += characters[randomPassword];
+  }
+  showResult.value = result;
+  console.log(result);
+}
